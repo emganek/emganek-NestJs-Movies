f61d11a2e9170ad62e4803e4e5b158b1a20e925c
Hiep Nguyen <hiep.n@tecalliance.net>
2024-06-27 18:13:29 +0700

    change config

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({    
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true
-  });
+  app.enableCors();
   app.useGlobalFilters();
   app.useGlobalPipes(
     new ValidationPipe({
